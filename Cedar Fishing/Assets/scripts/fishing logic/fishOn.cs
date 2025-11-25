d4da6623fe9b5685563d40e262828d964e217f12
RyanRiepma <ryanriepma@cedarville.edu>
2025-11-25 18:51:29 -0500

    Working Journal

--- Cedar Fishing/Assets/scripts/fishing logic/fishOn.cs
+++ Cedar Fishing/Assets/scripts/fishing logic/fishOn.cs
@@ -19,27 +19,48 @@ public class fishOn : MonoBehaviour
     Animator animator;
 
     public GameObject Anchovy;
+    public GameObject AnchovyImage;
     public GameObject BarredKnifeJaw;
+    public GameObject BarredKnifeJawImage;
     public GameObject Bitterling;
+    public GameObject BitterlingImage;
     public GameObject BlackBass;
+    public GameObject BlackBassImage;
     public GameObject BlueGill;
+    public GameObject BlueGillImage;
     public GameObject Carp;
+    public GameObject CarpImage;
     public GameObject CrucianCarp;
+    public GameObject CrucianCarpImage;
     public GameObject Dace;
+    public GameObject DaceImage;
     public GameObject FreshWaterGoby;
+    public GameObject FreshWaterGobyImage;
     public GameObject HorseMackeral;
+    public GameObject HorseMackeralImage;
     public GameObject Koi;
+    public GameObject KoiImage;
     public GameObject Loach;
+    public GameObject LoachImage;
     public GameObject OarFish;
+    public GameObject OarFishImage;
     public GameObject OliveFlounder;
+    public GameObject OliveFlounderImage;
     public GameObject PaleChub;
+    public GameObject PaleChubImage;
     public GameObject PondSmelt;
+    public GameObject PondSmeltImage;
     public GameObject RedSnapper;
+    public GameObject RedSnapperImage;
     public GameObject Salmon;
+    public GameObject SalmonImage;
     public GameObject SeaBass;
+    public GameObject SeaBassImage;
     public GameObject YellowPerch;
+    public GameObject YellowPerchImage;
 
     private GameObject fish;
+    private GameObject fishImage;
 
     private int clickCount = 0;
     private int clickThreshold = 5;
@@ -87,82 +108,102 @@ public class fishOn : MonoBehaviour
                 if (number == 1)
                 {
                     fish = Anchovy;
+                    fishImage = AnchovyImage;
                 }
                 if (number == 2)
                 {
                     fish = BarredKnifeJaw;
+                    fishImage = BarredKnifeJawImage;
                 }
                 if (number == 3)
                 {
                     fish = Bitterling;
+                    fishImage = BitterlingImage;
                 }
                 if (number == 4)
                 {
                     fish = BlackBass;
+                    fishImage = BlackBassImage;
                 }
                 if (number == 5)
                 {
                     fish = BlueGill;
+                    fishImage = BlueGillImage;
                 }
                 if (number == 6)
                 {
                     fish = Carp;
+                    fishImage = CarpImage;
                 }
                 if (number == 7)
                 {
                     fish = CrucianCarp;
+                    fishImage= CrucianCarpImage;
                 }
                 if (number == 8)
                 {
                     fish = Dace;
+                    fishImage = DaceImage;
                 }
                 if (number == 9)
                 {
                     fish = FreshWaterGoby;
+                    fishImage = FreshWaterGobyImage;
                 }
                 if (number == 10)
                 {
                     fish = HorseMackeral;
+                    fishImage = HorseMackeralImage;
                 }
                 if (number == 11)
                 {
                     fish = Koi;
+                    fishImage = KoiImage;
                 }
                 if (number == 12)
                 {
                     fish = Loach;
+                    fishImage = LoachImage;
                 }
                 if (number == 13)
                 {
                     fish = OarFish;
+                    fishImage = OarFishImage;
                 }
                 if (number == 14)
                 {
                     fish = OliveFlounder;
+                    fishImage = OliveFlounderImage;
                 }
                 if (number == 15)
                 {
                     fish = PaleChub;
+                    fishImage = PaleChubImage;
                 }
                 if (number == 16)
                 {
                     fish = PondSmelt;
+                    fishImage = PondSmeltImage;
                 }
                 if (number == 17)
                 {
                     fish = RedSnapper;
+                    fishImage = RedSnapperImage;
                 }
                 if (number == 18)
                 {
                     fish = Salmon;
+                    fishImage = SalmonImage;
                 }
                 if (number == 19)
                 {
                     fish = SeaBass;
+                    fishImage = SeaBassImage;
                 }
                 if (number == 20)
                 {
                     fish = YellowPerch;
+                    fishImage = YellowPerchImage;
                 }
 
                 cast.enabled = false;
@@ -230,6 +271,8 @@ public class fishOn : MonoBehaviour
                 timer2 = 0;
 
                 Debug.Log("Fish Caught");
+                fishImage.SetActive(true);
+
 
                 fish.SetActive(false);
                 line.SetActive(false);
